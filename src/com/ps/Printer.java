6d2dd3a478d14bea8d616bb2b18b4650f570bf97
dreamvalidator <78457331+dreamvalidator@users.noreply.github.com>
2022-04-07 11:22:50 +0530

    Update Printer.java

--- src/com/ps/Printer.java
+++ src/com/ps/Printer.java
@@ -1,11 +1,11 @@
 package com.ps;
 public class Printer {
-	private static Printer INSTANCE;
+	/*private static Printer INSTANCE;*/
 	
 	private Printer() {
 		System.out.println("Printer::0-param constructor");
 	}
-	public static Printer getInsatnce() {
+	/*public static Printer getInsatnce() {
 		if(INSTANCE==null) {
 		synchronized (Printer.class) {
 			if(INSTANCE==null) 
@@ -15,6 +15,12 @@ public class Printer {
 		}
 		return INSTANCE;
 		
+	}*/
+	private static class InnerPrinter {
+		private static volatile Printer INSTANCE=new Printer();
+	}
+	public static Printer getInstance() {
+		return InnerPrinter.INSTANCE;
 	}
 	public void printMessage(String mesg) {
 		
