3b1ae00752c489b791bf057b81bfedb8d472bf5a
dreamvalidator <itscoolpravin@gmail.com>
2022-04-07 11:03:20 +0530

    object egar initalise as volatile

--- src/com/ps/Printer.java
+++ src/com/ps/Printer.java
@@ -1,20 +1,26 @@
 package com.ps;
 public class Printer {
-	private static Printer INSTANCE;
-	
+	/*
+	 * private static Printer INSTANCE;
+	 */	
 	private Printer() {
 		System.out.println("Printer::0-param constructor");
 	}
-	public static Printer getInsatnce() {
-		if(INSTANCE==null) {
-		synchronized (Printer.class) {
-			if(INSTANCE==null) 
-				INSTANCE=new Printer();
-			
-		}
-		}
-		return INSTANCE;
-		
+
+	/*
+	 * public static Printer getInsatnce() { if(INSTANCE==null) {
+	 *  synchronized
+	 * (Printer.class) { if(INSTANCE==null) INSTANCE=new Printer();
+	 * 
+	 * } } return INSTANCE;
+	 * 
+	 * }
+	 */
+	private static class InnerPrinter {
+		private static volatile Printer INSTANCE=new Printer();
+	}
+	public static Printer getInstance() {
+		return InnerPrinter.INSTANCE;
 	}
 	public void printMessage(String mesg) {
 		
